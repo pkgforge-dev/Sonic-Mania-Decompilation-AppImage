41ebb4297013aa99bb8cfa78c1e446cd69fe783e
Link4Electronics <link4electronics@gmail.com>
2026-01-23 13:48:38 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/RSDKv5U /usr/lib/libdecor/*/libdecor-cairo.so
+quick-sharun /usr/bin/RSDKv5U
 
 # Additional changes can be done in between here
 
