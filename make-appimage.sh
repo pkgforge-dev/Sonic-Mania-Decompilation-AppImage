2f47758c30588801beb6d92ce2c84bed2b75a3f0
Link4Electronics <link4electronics@gmail.com>
2026-01-23 21:09:14 -0300

    remove pipewire

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,6 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/switch-icon.jpg
 export DESKTOP=/usr/share/applications/sonicmania.desktop
 export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/RSDKv5U
