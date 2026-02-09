107e37ef0c51b7c5508ed14aea93aabb2a76ead4
Link4Electronics <link4electronics@gmail.com>
2026-02-09 15:31:52 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,6 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-#export ICON=/usr/share/icons/switch-icon.jpg
-#export DESKTOP=/usr/share/applications/sonicmania.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
