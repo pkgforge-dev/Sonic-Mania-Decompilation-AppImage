c189655dee705d47a6c61c7c68731bdbcf6f22b8
Link4Electronics <link4electronics@gmail.com>
2026-08-14 10:43:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,6 +18,18 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package sonicmania-git
+#make-aur-package sonicmania-git
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of Sonic Mania Decompilation..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/RSDKModding/Sonic-Mania-Decompilation"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./SonicMania
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./SonicMania
+cmake -S ./ -B build -D CMAKE_BUILD_TYPE=Release -D RETRO_DISABLE_PLUS=OFF
+cmake --build build -j$(nproc)
+mv -v ./build/dependencies/RSDKv5/RSDKv5U ./build/dependencies/RSDKv5/libGame.so ../AppDir/bin
