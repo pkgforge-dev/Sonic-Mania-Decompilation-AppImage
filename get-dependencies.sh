c761d4aa2bfc1c3661eda33257a4f0f5981bd655
Link4Electronics <link4electronics@gmail.com>
2026-08-14 11:51:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,23 +7,17 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake     \
-    glew      \
-    glfw      \
-    libdecor  \
-    libtheora \
-    sdl2      \
-    tinyxml2
+    cmake       \
+    glew        \
+    glfw        \
+    libtheora   \
+    sdl2-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package sonicmania-git
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of Sonic Mania Decompilation..."
+echo "Building Sonic Mania Decompilation..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/RSDKModding/Sonic-Mania-Decompilation"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
@@ -31,7 +25,6 @@ git clone --recursive --depth 1 "$REPO" ./SonicMania
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-cd ./SonicMania
-cmake -S ./ -B build -D CMAKE_BUILD_TYPE=Release -D RETRO_DISABLE_PLUS=OFF -DUSE_SDL_AUDIO=ON
+cmake -S ./SonicMania -B build -DCMAKE_BUILD_TYPE=Release -DRETRO_DISABLE_PLUS=OFF -DUSE_SDL_AUDIO=ON
 cmake --build build -j$(nproc)
-mv -v ./build/dependencies/RSDKv5/RSDKv5U ./build/dependencies/RSDKv5/libGame.so ../AppDir/bin
+mv -v ./build/dependencies/RSDKv5/RSDKv5U ./build/dependencies/RSDKv5/libGame.so ./AppDir/bin
