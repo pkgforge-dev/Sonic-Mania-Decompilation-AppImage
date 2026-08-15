ca298d9db85b97693e5217005fe9d6db7303a670
Link4Electronics <link4electronics@gmail.com>
2026-08-15 20:30:58 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,10 +18,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of Sonic Mania Decompilation..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/RSDKModding/Sonic-Mania-Decompilation"
