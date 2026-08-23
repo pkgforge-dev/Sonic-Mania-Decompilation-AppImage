d26c966f3fdd005430efd28bfa93fa99b1da01fb
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:10:47 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,13 +10,12 @@ pacman -Syu --noconfirm \
     cmake     \
     glew      \
     glfw      \
-    libdecor  \
     libtheora \
     sdl2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 echo "Building Sonic Mania Decompilation..."
 echo "---------------------------------------------------------------"
